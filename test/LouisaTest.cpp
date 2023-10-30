7ea79f4577ffdded2bb98daa6be20e67e8e583ea
LouisaPawusch <st162850@stud.uni-stuttgart.de>
2023-10-30 14:10:37 -0400

    made the .py to a .cpp

---
+++ test/LouisaTest.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cstdlib>
+
+int main(int argc, char *argv[])
+{
+  // if the number of given command line arguments is only 1 (= the program name), print out usage information and exit
+  if (argc == 1)
+  {
+    std::cout << "usage: " << argv[0] << " <filename>" << std::endl;
+
+    return EXIT_FAILURE;
+  }
+
+  // read in the first argument
+  std::string filename = argv[1];
+
+  // print message
+  std::cout << "Filename: \"" << filename << "\"" << std::endl;
+
+  return EXIT_SUCCESS;
+}
