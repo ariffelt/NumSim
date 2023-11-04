f3cf573405715ca291a361ada9db319ed5be1406
mkosiol <mn_github@kosiol.net>
2023-11-05 00:22:17 +0100

    Code Snippets: Object orientation +new folder name

---
+++ test/maureen/objectOrientation/storage/fieldvariable.h
@@ -0,0 +1,10 @@
+/** A field variable is the discretization of a scalar function f(x) with x in the computational domain.
+ *  More specifically, a scalar value is stored at discrete nodes/points. The nodes are arranged in an equidistant mesh
+ *  with specified mesh width.
+ */
+
+class FieldVariable :
+    public Array2D
+{
+    ...
+};
