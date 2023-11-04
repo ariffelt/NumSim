a15007ce8f5e5a9cb79e0719c9207a6954de5d3a
mkosiol <mn_github@kosiol.net>
2023-11-04 17:03:56 +0100

    Finished exercise: Setting up the build system

---
+++ test/maureen/cmakeTests/build/src/CMakeFiles/numsim.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/maureen/NumSim/gitNumSim/NumSim/test/maureen/cmakeTests/src/main.cpp" "src/CMakeFiles/numsim.dir/main.cpp.o" "gcc" "src/CMakeFiles/numsim.dir/main.cpp.o.d"
+  "/home/maureen/NumSim/gitNumSim/NumSim/test/maureen/cmakeTests/src/output_writer/write_paraview_output.cpp" "src/CMakeFiles/numsim.dir/output_writer/write_paraview_output.cpp.o" "gcc" "src/CMakeFiles/numsim.dir/output_writer/write_paraview_output.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ test/maureen/cmakeTests/build/src/CMakeFiles/numsim.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/numsim.dir/main.cpp.o"
+  "CMakeFiles/numsim.dir/main.cpp.o.d"
+  "CMakeFiles/numsim.dir/output_writer/write_paraview_output.cpp.o"
+  "CMakeFiles/numsim.dir/output_writer/write_paraview_output.cpp.o.d"
+  "numsim"
+  "numsim.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/numsim.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
