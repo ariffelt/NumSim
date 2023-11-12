ca231df7d89d7484a3bb8fc595a768eae8107150
ariffelt <st161905@stud.uni-stuttgart.de>
2023-11-12 17:55:43 +0100

    Fixed misspelling in pressure solver and Gauss-Seidel and implemented parts in computation.

--- src/pressure_solver/gauss_seidel.cpp
+++ src/pressure_solver/gauss_seidel.cpp
@@ -11,8 +11,8 @@ Gauss_Seidel::Gauss_Seidel(std::shared_ptr<Discretization> discretization,
 
 void Gauss_Seidel::solve()
 {
-    const double hx2 = discretization_->meshWidth_[0];
-    const double hy2 = discretization_->meshWidth_[1];
+    const double hx2 = discretization_->dx() * discretization_->dx();
+    const double hy2 = discretization_->dy() * discretization_->dy();
     const double prefactor = hx2 * hy2 / (2 * (hx2 + hy2));
 
     // compute initial residual
@@ -25,11 +25,11 @@ void Gauss_Seidel::solve()
 
     while (res2 >= epsilon2 * res02 && iteration < maximumNumberOfIterations_)
     { // Implement GS for inner points
-        for (i = discretisation_->pIBegin() + 1; i < discretisation_->pIEnd() - 1; i++)
+        for (i = discretization_->pIBegin() + 1; i < discretization_->pIEnd() - 1; i++)
         {
-            for (j = discretisation_->pJBegin() + 1; j < discretisation_->pJEnd() - 1; j++)
+            for (j = discretization_->pJBegin() + 1; j < discretization_->pJEnd() - 1; j++)
             {
-                    discretisation_ -> p(i,j) = prefactor * ((discretisation_ -> p(i-1,j) + discretisation_ -> p(p+1,j))/(hx2) + (discretisation_ -> p(i,j-1) + discretisation_ -> p(i,j+1))/(hy2)) - discretisation_ -> rhs(i,j));
+                    discretization_ -> p(i,j) = prefactor * ((discretization_ -> p(i-1,j) + discretization_ -> p(p+1,j))/(hx2) + (discretization_ -> p(i,j-1) + discretization_ -> p(i,j+1))/(hy2)) - discretization_ -> rhs(i,j));
             }
         }
         // Update boundary values such that Neumann Boundary conditions are fulfilled
--- src/pressure_solver/pressure_solver.cpp
+++ src/pressure_solver/pressure_solver.cpp
@@ -12,30 +12,30 @@ PressureSolver(std::shared_ptr<Discretization> discretization, double epsilon, i
 
 double PressureSolver::getResidual(){
     double res = 0;
-    for (i = discretisation_->pIbegin() + 1; i < discretisation_->pIEnd() - 1; i++)
+    for (i = discretization_->pIbegin() + 1; i < discretization_->pIEnd() - 1; i++)
     {
-        for (j = discretisation_->pJbegin() + 1; j < discretisation_->pIEnd() - 1; j++)
+        for (j = discretization_->pJbegin() + 1; j < discretization_->pJEnd() - 1; j++)
         {
             // Second order derivative of p in direction x
-            double d2pdx2 = (discretisation_->p(i - 1, j) - 2 * discretisation_->p(i, j) + discretisation_->p(i + 1, j)) / (hx2);
+            double d2pdx2 = (discretization_->p(i - 1, j) - 2 * discretization_->p(i, j) + discretization_->p(i + 1, j)) / (hx2);
             // Second order derivative of p in direction y
-            double d2pdy2 = (discretisation_->p(i, j - 1) - 2 * discretisation_->p(i, j) + discretisation_->p(i, j + 1)) / (hy2);
+            double d2pdy2 = (discretization_->p(i, j - 1) - 2 * discretization_->p(i, j) + discretization_->p(i, j + 1)) / (hy2);
             // compute residual
-            res02 += (d2pdx2 + d2pdy2 - discretization_->rhs(i, j)) * (d2pdx2 + d2pdy2 - discretization_->rhs(i, j));
+            res += (d2pdx2 + d2pdy2 - discretization_->rhs(i, j)) * (d2pdx2 + d2pdy2 - discretization_->rhs(i, j));
         }
     }
-    return res;
+    return res/(discretization_ -> nCells[0]*nCells[1]);
 }
 
 void PressureSolver::setBoundaryValues(){
-    for (i = discretisation_->pIBegin(); i < discretisation_->pIEnd(); i++)
+    for (i = discretization_->pIBegin(); i < discretization_->pIEnd(); i++)
         {
-            discretisation_->p(i, discretisation_->pJEnd() - 1) = discretisation_->p(i, discretisation_->pJEnd() - 2);
-            discretisation_->p(i, discretisation_->pJBegin()) = discretisation_->p(i, discretisation_->pJBegin + 1);
+            discretization_->p(i, discretization_->pJEnd() - 1) = discretization_->p(i, discretization_->pJEnd() - 2);_
+            discretization_->p(i, discretization_->pJBegin()) = discretization_->p(i, discretization_->pJBegin + 1);
         }
-        for (j = discretisation_->pJBegin(); i < discretisation_->pJEnd(); j++)
+        for (j = discretization_->pJBegin(); i < discretization_->pJEnd(); j++)
         {
-            discretisation_->p(discretisation_->pIEnd() - 1, j) = discretisation_->p(discretisation_->pIEnd() - 2, j);
-            discretisation_->p(discretisation_->pIBegin(), j) = discretisation_->p(discretisation_->pIBegin + 1, j);
+            discretization_->p(discretization_->pIEnd() - 1, j) = discretization_->p(discretization_->pIEnd() - 2, j);
+            discretization_->p(discretization_->pIBegin(), j) = discretization_->p(discretization_->pIBegin + 1, j);
         }
 }
