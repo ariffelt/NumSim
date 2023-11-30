f81f6e6f71daa2bae30d915532a560628ebd0090
LouisaPawusch <st162850@stud.uni-stuttgart.de>
2023-11-30 13:40:46 +0100

    add includes in computation.h to used outputWriter

--- src/computation/computation.h
+++ src/computation/computation.h
@@ -5,8 +5,10 @@
 #include "settings/settings.h"
 #include "discretization/1_discretization.h"
 #include "pressure_solver/pressure_solver.h"
-#include "output_writer/output_writer_paraview.h"
-#include "output_writer/output_writer_text.h"
+//#include "output_writer/output_writer_paraview.h"
+//#include "output_writer/output_writer_text.h"
+#include "output_writer/output_writer_paraview_parallel.h"
+#include "output_writer/output_writer_text_parallel.h"
 
 class Computation
 {
